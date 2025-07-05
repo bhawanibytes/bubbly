2b95f06248ddc43181d5a0a7e563f84cbff4a653
Bhawani Singh <bs190520@gmail.com>
2025-07-06 03:24:38 +0530

    Added header attaching and status code logic

--- ws/src/utils/async.cork.ts
+++ ws/src/utils/async.cork.ts
@@ -15,8 +15,25 @@ export function withSafeAsync(asyncHandler: (
         const result = await asyncHandler( res, req);
         if (!aborted) {
           res.cork(() => {
-            res.writeHeader("Content-Type", "application/json");
-            res.end(JSON.stringify(result));
+            if (result?.data?.headers){
+              // set headers to res if there is received any from handler
+              for (const key in result?.data?.headers){
+                if (Object.prototype.hasOwnProperty.call(result?.data?.headers, key)) {
+                  res.writeHeader(`${key}`, `${result?.data?.headers[key]}` );
+                }
+              }
+              // delete headers once they are attached
+              delete result?.data?.headers
+              
+              res.writeStatus(`${result?.data?.status}`);
+              res.end(JSON.stringify(result));
+            } else {
+              res.writeHeader("Content-Type", "application/json" );
+              if (result?.data?.status) {
+                res.writeStatus(result.data.status)
+              }
+              res.end(JSON.stringify(result));
+            }
           });
         }
       } catch (err) {
