5fe695c8e4d94d5f10a438ec14692adbca7cbfa7
Bhawani Singh <bs190520@gmail.com>
2025-07-06 03:27:05 +0530

    added hashing, JWT, Status Codes, login and signup done

--- ws/src/controllers/auth.controllers.ts
+++ ws/src/controllers/auth.controllers.ts
@@ -1,67 +1,151 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 import { eq } from "drizzle-orm";
 import { db } from "../db/db.js";
 import { users } from "../db/schema/users.js";
 import { UWSReq, UWSRes } from "../types/types.uws.js";
 import { jsonBody } from "../utils/jsonBody.js";
+import { rounds, secret } from "../configs/env.config.js";
+import { Result } from "../types/type.res.js";
 
-interface TypeBody {
+interface SignupBody {
   name: string;
+  number: string;
+  pin: string;
+}
+
+interface LoginBody {
+  number: string;
+  pin: string;
+}
+
+interface PinBody {
   number: string;
 }
 
 export async function signup( res: UWSRes, req: UWSReq ) {
   try {
-    const body =  await jsonBody(res) as unknown as TypeBody;
-    const { name, number } = body
-    if (!name || !number) {
-      return { status: "failed", message: "all arg required" };
+    const body =  await jsonBody(res) as SignupBody;
+    const { name, number, pin } = body
+
+    // return if all details are not provided
+    if (!name || !number || !pin) {
+      return { success: false, status: "400 Bad Request",  message: "All fields required", data: null } as Result
+    } 
+
+    // check if user already exists and returns
+    const userExists = await db
+      .select()
+      .from(users)
+      .where(eq(users.number, number));
+    if (userExists.length > 0) {
+      return { success: false, status: "409 Conflict" ,  message: "User already exists" } as Result
+    }
+
+    // hash pin and save user to db
+    const hashedPin = await bcrypt.hash(pin, rounds)
+    const [insertedUser] = await db
+      .insert(users)
+      .values({
+        name,
+        number,
+        pin: hashedPin,
+      })
+      .returning({ number: users.number })
+    
+    // if user saved send success else send error
+    if (insertedUser.number == number) {
+      return { success: true, status: "201 Created" , message: "You signed up", data: null } as Result
     } else {
-      const userExists = await db
-        .select()
-        .from(users)
-        .where(eq(users.number, number));
-      if (userExists.length > 0) {
-        return { status: "failed", message: "user already exists" };
-      } else {
-        const [insertedUser] = await db
-          .insert(users)
-          .values({
-            name,
-            number,
-          })
-          .returning({ id: users.number });
-        if (insertedUser.id == number) {
-          return { status: "OK", message: "You signed up" };
-        } else {
-          return { status: "Failed", message: "Something wrong while saving" };
-        }
-      }
+      return { success: false, status: "500 Internal Server Error", message: "Something wrong while saving", data: null } as Result
     }
   } catch (error) {
-    return { status: "failed", message: error };
+    console.log(error)
+    return { success: false, status: "500 Internal Server Error", message: "Something Went Wrong", error } as Result
   }
 }
 
 export async function login(res: UWSRes, req: UWSReq ) {
   try {
-    const body = await jsonBody(res) as unknown as TypeBody;
-    const { number } = body;
-    if (!number) {
-      return { status: "failed", message: "all arg required" };
-    } else {
-      const [result] = await db
-        .select()
-        .from(users)
-        .where(eq(users.number, number));
-      if (number == result.number) {
-        return { status: "OK", message: "logged in" };
-      }
+    const body = await jsonBody(res) as LoginBody;
+    const { number, pin } = body;
+
+    // return if all details are not provided
+    if (!number || !pin) {
+      return { success: false, status: "400 Bad Request", message: "All fields required", error: null } as Result
     }
+
+    // retrieve details from db
+    const [result] = await db
+      .select()
+      .from(users)
+      .where(eq(users.number, number))
+
+    //return if user don't exists      
+    if(!result) {
+      return { success: false, status: "401 Unauthorized", message: "Invalid credentials", data: null} as Result
+    } 
+
+    //match pin with hashed pin
+    const pinMatch = await bcrypt.compare(pin, result?.pin)
+    
+    // error if pin not matched else return token
+    if (!pinMatch) {
+      return { 
+        success: false,
+        status: "401 Unauthorized",
+        message: "Invalid credentials",
+        error: {
+          code: "INVALID_CREDENTIALS",
+          details: "The username or password you entered is incorrect."
+        }
+      } as Result
+    } else {
+      const token = jwt.sign(result.id+"", secret)
+      return { 
+        success: true, 
+        status: "200 OK"  ,
+        message: "Logged in successfully", 
+        data: {
+          token,
+          headers : {
+            Authorization: `Bearer ${token}`
+          },
+        }
+      } as Result
+    }
+    
   } catch (error) {
-    return { status: "failed", message: error };
+    console.log(error)
+    return { success: false, message: "Something Went Wrong", error, } as Result
   }
 }
 
-export async function forgetPasscode(res: UWSRes, req: UWSReq) {
+export async function forgetPin(res: UWSRes, req: UWSReq) {
+  try {
+    const body = await jsonBody(res) as PinBody
+    const { number } = body
+    // return if number are not provided
+    if (!number) {
+      return { success: false, status: "400 Bad Request", message: "All fields required", error: null } as Result
+    }
+
+    // retrieve details from db
+    const [result] = await db
+      .select()
+      .from(users)
+      .where(eq(users.number, number))
+
+    //return if user don't exists      
+    if(!result) {
+      return { success: false, status: "401 Unauthorized", message: "Invalid credentials", data: null} as Result
+    }
+
+    result.number
+    
+  } catch (error) {
+    
+  }
+
   // ...
 }
