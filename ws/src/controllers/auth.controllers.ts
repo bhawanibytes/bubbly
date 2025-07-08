d6e0710ecb239f0e30fd0c1035149f013a9a365f
Bhawani Singh <bs190520@gmail.com>
2025-07-09 03:20:11 +0530

    added combined asyncJson handler and added JWT Expiry using ms

--- ws/src/controllers/auth.controllers.ts
+++ ws/src/controllers/auth.controllers.ts
@@ -1,11 +1,10 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { eq } from "drizzle-orm";
 import { db } from "../db/db.js";
 import { users } from "../db/schema/users.js";
 import { UWSReq, UWSRes } from "../types/types.uws.js";
-import { jsonBody } from "../utils/jsonBody.js";
-import { rounds, secret } from "../configs/env.config.js";
+import { accessExpiry, refreshExpiry, rounds, secret } from "../configs/env.config.js";
 import { Result } from "../types/type.res.js";
 
 interface SignupBody {
@@ -23,128 +22,180 @@ interface PinBody {
   number: string;
 }
 
-export async function signup( res: UWSRes, req: UWSReq ) {
+export async function signup(
+  res: UWSRes,
+  req: UWSReq,
+  body: any
+): Promise<any> {
+  const { name, number, pin } = body as SignupBody;
+  // return if all details are not provided
+  if (!name || !number || !pin) {
+    return {
+      success: false,
+      status: "400 Bad Request",
+      message: "All fields required",
+      data: null,
+    } as Result;
+  }
   try {
-    const body =  await jsonBody(res) as SignupBody;
-    const { name, number, pin } = body
-
-    // return if all details are not provided
-    if (!name || !number || !pin) {
-      return { success: false, status: "400 Bad Request",  message: "All fields required", data: null } as Result
-    } 
-
     // check if user already exists and returns
     const userExists = await db
       .select()
       .from(users)
       .where(eq(users.number, number));
     if (userExists.length > 0) {
-      return { success: false, status: "409 Conflict" ,  message: "User already exists" } as Result
+      return {
+        success: false,
+        status: "409 Conflict",
+        message: "User already exists",
+      } as Result;
     }
 
     // hash pin and save user to db
-    const hashedPin = await bcrypt.hash(pin, rounds)
-    const [insertedUser] = await db
-      .insert(users)
-      .values({
-        name,
-        number,
-        pin: hashedPin,
-      })
-      .returning({ number: users.number })
-    
-    // if user saved send success else send error
-    if (insertedUser.number == number) {
-      return { success: true, status: "201 Created" , message: "You signed up", data: null } as Result
-    } else {
-      return { success: false, status: "500 Internal Server Error", message: "Something wrong while saving", data: null } as Result
-    }
+    const hashedPin = await bcrypt.hash(pin, rounds);
+    const [insertedUser] = await db.insert(users).values({
+      name,
+      number,
+      pin: hashedPin,
+    });
+
+    // in end return the response once user is created
+    return {
+      success: true,
+      status: "201 Created",
+      message: "You signed up",
+      data: null,
+    } as Result;
   } catch (error) {
-    console.log(error)
-    return { success: false, status: "500 Internal Server Error", message: "Something Went Wrong", error } as Result
+    console.log(error);
+    return {
+      success: false,
+      status: "500 Internal Server Error",
+      message: "Something Went Wrong",
+      error,
+    } as Result;
   }
 }
 
-export async function login(res: UWSRes, req: UWSReq ) {
+export async function login(res: UWSRes, req: UWSReq, body: any): Promise<any> {
+  const { number, pin } = body as LoginBody;
+
+  // return if all details are not provided
+  if (!number || !pin) {
+    return {
+      success: false,
+      status: "400 Bad Request",
+      message: "All fields required",
+      error: null,
+    } as Result;
+  }
   try {
-    const body = await jsonBody(res) as LoginBody;
-    const { number, pin } = body;
-
-    // return if all details are not provided
-    if (!number || !pin) {
-      return { success: false, status: "400 Bad Request", message: "All fields required", error: null } as Result
-    }
-
     // retrieve details from db
     const [result] = await db
       .select()
       .from(users)
-      .where(eq(users.number, number))
+      .where(eq(users.number, number));
 
-    //return if user don't exists      
-    if(!result) {
-      return { success: false, status: "401 Unauthorized", message: "Invalid credentials", data: null} as Result
-    } 
+    //return if user don't exists
+    if (!result) {
+      return {
+        success: false,
+        status: "401 Unauthorized",
+        message: "Invalid credentials",
+        data: null,
+      } as Result;
+    }
 
     //match pin with hashed pin
-    const pinMatch = await bcrypt.compare(pin, result?.pin)
-    
+    const pinMatch = await bcrypt.compare(pin, result?.pin);
+
     // error if pin not matched else return token
     if (!pinMatch) {
-      return { 
+      return {
         success: false,
         status: "401 Unauthorized",
         message: "Invalid credentials",
         error: {
           code: "INVALID_CREDENTIALS",
-          details: "The username or password you entered is incorrect."
-        }
-      } as Result
+          details: "The username or password you entered is incorrect.",
+        },
+      } as Result;
     } else {
-      const token = jwt.sign(result.id+"", secret)
-      return { 
-        success: true, 
-        status: "200 OK"  ,
-        message: "Logged in successfully", 
-        data: {
-          token,
-          headers : {
-            Authorization: `Bearer ${token}`
-          },
+      const accessToken = jwt.sign(
+        {
+          id: result.id,
+        },
+        secret,
+        {
+          expiresIn: accessExpiry,
         }
-      } as Result
+      );
+      const refreshToken = jwt.sign(
+        {
+          id: result.id,
+        },
+        secret,
+        {
+          expiresIn: refreshExpiry,
+        }
+      );
+      return {
+        success: true,
+        status: "200 OK",
+        message: "Logged in successfully",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        data: {
+          accessToken,
+          refreshToken,
+        },
+      } as Result;
     }
-    
   } catch (error) {
-    console.log(error)
-    return { success: false, message: "Something Went Wrong", error, } as Result
+    console.log(error);
+    return { success: false, message: "Something Went Wrong", error } as Result;
   }
 }
 
-export async function forgetPin(res: UWSRes, req: UWSReq) {
+export async function forgetPin(
+  res: UWSRes,
+  req: UWSReq,
+  body: any
+): Promise<any> {
   try {
-    const body = await jsonBody(res) as PinBody
-    const { number } = body
+    const { number } = body;
     // return if number are not provided
     if (!number) {
-      return { success: false, status: "400 Bad Request", message: "All fields required", error: null } as Result
+      return {
+        success: false,
+        status: "400 Bad Request",
+        message: "All fields required",
+        error: null,
+      } as Result;
     }
 
     // retrieve details from db
     const [result] = await db
       .select()
       .from(users)
-      .where(eq(users.number, number))
+      .where(eq(users.number, number));
 
-    //return if user don't exists      
-    if(!result) {
-      return { success: false, status: "401 Unauthorized", message: "Invalid credentials", data: null} as Result
+    //return if user don't exists
+    if (!result) {
+      return {
+        success: false,
+        status: "401 Unauthorized",
+        message: "Invalid credentials",
+        data: null,
+      } as Result;
     }
+    
 
-    result.number
-    
+    result.number;
   } catch (error) {
-    
+    console.log(error);
+    return { success: false, message: "Something Went Wrong", error } as Result;
   }
 
   // ...
---
+++ ws/src/utils/asyncJson.ts
@@ -0,0 +1,63 @@
+import { Result } from "../types/type.res.js";
+import { UWSReq, UWSRes } from "../types/types.uws.js";
+
+export default function safeAsynceJson<T = any>(
+  handler: (res: UWSRes, req: UWSReq, body: T) => Promise<Result>
+) {
+  return (res: UWSRes, req: UWSReq) => {
+    let aborted = false;
+    let buffer = Buffer.alloc(0);
+    res.onAborted(() => {
+      aborted = true;
+      console.warn("⚠️ Request aborted by client");
+    });
+    res.onData((ab, isLast) => {
+      // stores ArrayBuffer
+      let chunk = Buffer.from(ab);
+      buffer = Buffer.concat([buffer, chunk]);
+      if (isLast) {
+        let body: T;
+        //parse body from buffer
+        try {
+          body = JSON.parse(buffer.toString());
+        } catch (error) {
+          res.writeStatus("400 Bad Request").end("Invalid JSON");
+          return;
+        }
+        // calls handler with body, IIFE
+        (async () => {
+          try {
+            const result = await handler(res, req, body);
+            if (aborted || !result) return;
+            // write all header and json response in batch
+            res.cork(() => {
+              res.writeHeader("Content-Type", "application/json");
+              if (result.headers) {
+                for (const key in result.headers) {
+                  res.writeHeader(key, result.headers[key]);
+                }
+                delete result.headers;
+              }
+              res.writeStatus(result.status);
+              res.end(JSON.stringify(result));
+            });
+          } catch (err) {
+            console.error("❌ Handler error:", err);
+            if (!aborted) {
+              res.cork(() => {
+                res.writeStatus("500 Internal Server Error");
+                res.end(
+                  JSON.stringify({
+                    success: false,
+                    status: "500 Internal Server Error",
+                    message: "Internal Server Error",
+                  } as Result)
+                );
+              });
+            }
+          }
+        })();
+      }
+    });
+  };
+}
