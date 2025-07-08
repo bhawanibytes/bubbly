82dffa91e7fba464580fd7069d93707e45195e3c
Bhawani Singh <bs190520@gmail.com>
2025-07-09 02:25:55 +0530

    modified the Async and Json handlers

--- ws/src/routes/auth.routes.ts
+++ ws/src/routes/auth.routes.ts
@@ -4,10 +4,10 @@ import {
   signup,
 } from "../controllers/auth.controllers.js";
 import { AppType } from "../types/types.uws.js";
-import { withSafeAsync } from "../utils/async.cork.js";
+import safeAsynceJson from "../utils/asyncJson.js";
 
 export function registerAuthRoutes(app: AppType) {
-  app.post("/signup", withSafeAsync(signup));
-  app.post("/login", withSafeAsync(login));
-  app.post("/forget-passcode", withSafeAsync(forgetPin));
+  app.post("/signup", safeAsynceJson(signup));
+  app.post("/login", safeAsynceJson(login));
+  app.post("/forget-pin", safeAsynceJson(forgetPin));
 }
