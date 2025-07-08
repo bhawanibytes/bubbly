6f16f301c86cfd95ad11890f9e5461e9c8b46ed0
Bhawani Singh <bs190520@gmail.com>
2025-07-09 03:14:52 +0530

    added access and refresh expiries

--- ws/src/configs/env.config.ts
+++ ws/src/configs/env.config.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config';
+import ms, { StringValue } from 'ms';
 
 // db url
 if (!process.env.DATABASE_URL) {
@@ -17,3 +18,15 @@ if (!process.env.JWT_SECRET){
     throw new Error ("JWT_SECRET are not in env")
 }
 export const secret = process.env.JWT_SECRET
+
+// JWT_ACCESS_TOKEN_EXPIRY
+if (!process.env.JWT_ACCESS_TOKEN_EXPIRY){
+    throw new Error ("JWT_ACCESS_TOKEN_EXPIRY are not in env")
+}
+export const accessExpiry = ms(process.env.JWT_ACCESS_TOKEN_EXPIRY as StringValue)
+
+// JWT_ACCESS_TOKEN_EXPIRY
+if (!process.env.JWT_REFRESH_TOKEN_EXPIRY){
+    throw new Error ("JWT_REFRESH_TOKEN_EXPIRY are not in env")
+}
+export const refreshExpiry = ms(process.env.JWT_REFRESH_TOKEN_EXPIRY as StringValue)
