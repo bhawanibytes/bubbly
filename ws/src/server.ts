6aa06e52dcf4e656c5e2cb7ee2a9db4ba61c062d
Bhawani Singh <bs190520@gmail.com>
2025-08-25 12:06:25 +0530

    added cors header code block for testing

--- ws/src/server.ts
+++ ws/src/server.ts
@@ -1,19 +1,22 @@
-import uWS from "uWebSockets.js";
-import { registerAuthRoutes } from "./routes/auth.routes.js";
-import { getCORSHeaders } from "./utils/cors.js";
+import uWS from "uWebSockets.js"
+import { registerAuthRoutes } from "./routes/auth.routes.js"
+import getCORSHeaders from "./utils/corsHeaders.js"
+import registerChatRoutes from "./routes/chats.routes.js"
+import logger from "./configs/logger.config.js"
 
-const port = 8080;
+const port = 8080
+const app = uWS.App()
 
-const app = uWS.App();
-
+// handle cors; write cors headers to preflight iteratively
 app.options("/*", (res, req) => {
-  const corsHeaders = getCORSHeaders(req);
+  const corsHeaders = getCORSHeaders(req)
   for (const key in corsHeaders) {
-    res.writeHeader(key, corsHeaders[key]);
+    res.writeHeader(key, corsHeaders[key])
   }
-  res.end("OK");
-});
+  res.end("OK")
+})
 
+// ws configs
 app.ws("/*", {
   // options
   compression: 0,
@@ -22,42 +25,49 @@ app.ws("/*", {
   // handlers
   open: (ws) => {
     // client subcribed to this topic "broadcast"
-    ws.subscribe("broadcast");
+    ws.subscribe("broadcast")
     // console.log(JSON.stringify( ws) + ` is conneccted`)
     // console.dir(ws)
     // console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(ws)));
     // console.log(ws.getRemoteAddressAsText())
     ws.send(
       " You are connected to serrver with this socket:" + JSON.stringify(ws),
-    );
+    )
   },
   message: (ws, message, isBinary) => {
     // broadcast the message to specific topic
-    ws.publish("broadcast", message, isBinary);
+    ws.publish("broadcast", message, isBinary)
   },
   drain: (ws) => {},
   close: (ws, code, message) => {
     // logic on unsubcribe
   },
-});
+})
 
-registerAuthRoutes(app);
+// register all routes
+registerAuthRoutes(app)
+registerChatRoutes(app)
+
+//
 app.post("/test", (res, req) => {
-  res.writeStatus("400 Bad Request");
-  res.writeHeader("Content-Type", "application/json");
-  res.end(JSON.stringify({ error: "Bad request" }));
-});
+  res.writeStatus("400 Bad Request")
+  res.writeHeader("Content-Type", "application/json")
+  res.end(JSON.stringify({ error: "Bad request" }))
+})
 
+// catch all unmatched or bad req
 app.any("/*", (res, req) => {
-  req;
-  res.end("notthing to see here");
-});
+  req
+  res.end("notthing to see here")
+})
 
+// port and server config
 app.listen(port, (token) => {
   if (token) {
-    console.log(`Listening to port ` + port);
-    // console.log('listenSocket value:', Object.keys(token));
+    // console.log(`Listening to port ` + port);
+    logger.info(`Listening to port ${port}`)
   } else {
-    console.log(`Failed to listen to port ` + port);
+    // console.log(`Failed to listen to port ` + port);
+    logger.info(`Failed to listen to port ${port}`)
   }
-});
+})
