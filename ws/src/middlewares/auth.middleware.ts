d5a5d725b74037cc8b042d4eeb8af1a574ba1a26
Bhawani Singh <bs190520@gmail.com>
2025-08-25 12:42:53 +0530

    added auth middlewares

---
+++ ws/src/middlewares/auth.middleware.ts
@@ -0,0 +1,29 @@
+import { secret } from "../configs/env.config"
+import { UWSReq, UWSRes } from "../types/types.uws"
+import logger from "../configs/logger.config"
+import cookieParser from "../utils/cookieParser"
+import jwt from "jsonwebtoken"
+
+export default function authMiddleware<T = any>(
+  next: (res: UWSRes, req: UWSReq) => void,
+) {
+  return (res: UWSRes, req: UWSReq) => {
+    try {
+      const cookies = cookieParser(req)
+      if (!cookies.accessToken) throw new Error("Access token missing")
+      const decoded = jwt.verify(cookies.accessToken, secret)
+      // @ts-ignore attach user to res
+      res.user = decoded
+      // call the handlers
+      next(res, req)
+    } catch (error: any) {
+      logger.error(`JWT Error: ${error.message}`, error)
+
+      if (error.name === "TokenExpiredError") {
+        res.writeStatus("401 Unauthorized").end("Token expired")
+      } else {
+        res.writeStatus("401 Unauthorized").end("Invalid token")
+      }
+    }
+  }
+}
