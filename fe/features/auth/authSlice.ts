b740e4f7e25fc76f10191c78bf2b5ad87b79c57f
Bhawani Singh <bs190520@gmail.com>
2025-10-08 16:02:30 +0530

    added User Number and Id from backend and restructed the configs for readability

--- fe/features/auth/authSlice.ts
+++ fe/features/auth/authSlice.ts
@@ -1,14 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+interface InitialState {
+  fullname: string;
+  number: string;
+  pin: number | null;
+  otpStatus: boolean;
+  otp: number | null;
+  userId: string;
+}
+const initialState: InitialState = {
+  fullname: "",
+  number: "",
+  pin: null,
+  otpStatus: false,
+  otp: null,
+  userId: "",
+};
 export const authSlice = createSlice({
   name: "auth",
-  initialState: {
-    fullname: null,
-    number: "",
-    pin: null,
-    otpStatus: false,
-    otp: null,
-  },
+  initialState,
   reducers: {
     signupFormData: (state, action) => {
       const { fullname, number, pin } = action.payload;
@@ -22,9 +31,16 @@ export const authSlice = createSlice({
     otpStatus: (state, action) => {
       state.otpStatus = action.payload.otpStatus;
     },
+    userDetailsFromBE: (state, action) => {
+      const { number, userId } = action.payload;
+      state.number = number;
+      state.userId = userId;
+      console.log(`number and userID from reducer ${number} : ${userId}`);
+    },
   },
 });
 
-export const { signupFormData, verifyFormData, otpStatus } = authSlice.actions;
+export const { signupFormData, verifyFormData, otpStatus, userDetailsFromBE } =
+  authSlice.actions;
 
 export default authSlice.reducer;
