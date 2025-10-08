8e8574d67b203a05eccba69b940566b36a59c293
Bhawani Singh <bs190520@gmail.com>
2025-10-08 15:42:21 +0530

    returning number and id of user on login

--- ws/src/controllers/auth.controllers.ts
+++ ws/src/controllers/auth.controllers.ts
@@ -225,7 +225,8 @@ export async function login(
           ],
         },
         data: {
-          number,
+          number: user.phoneNumber,
+          userId: user.id,
           accessToken,
           refreshToken,
         },
