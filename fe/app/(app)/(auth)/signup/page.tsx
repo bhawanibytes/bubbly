a708f5c43337cb0e9a70f56427a6f25c23a57eb7
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-05 18:58:24 +0530

    refactor: Update Dashboard component to enhance state management and integrate contact functionality

--- fe/app/(app)/(auth)/signup/page.tsx
+++ fe/app/(app)/(auth)/signup/page.tsx
@@ -1,20 +1,20 @@
 "use client";
 
-// import Image from "next/image";
+import Image from "next/image";
 import { RootState } from "@/redux/store";
 import OtpForm from "@components/OtpForm";
-import SignupForm from "@components/SignupForm"; 
+import SignupForm from "@components/SignupForm";
 import { useSelector } from "react-redux";
 import Button from "@components/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons"; 
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 export default function Signup() {
-  const otpStatus = useSelector((state: RootState) => state.auth.otpStatus);
+    const otpStatus = useSelector((state: RootState) => state.auth.otpStatus);
 
-  return (
-    <>
-      <div className="flex min-h-[91dvh] flex-col">
-        <div className="flex w-full justify-center gap-5">
+    return (
+        <>
+            <div className="flex min-h-[91dvh] flex-col">
+                {/* <div className="md:max-w-[70%] mx-auto outline outline-gray-400 flex w-full justify-center gap-50">
           <div
             className={`flex min-h-10 min-w-10 items-center justify-center rounded-full bg-gray-300`}
           >
@@ -25,24 +25,26 @@ export default function Signup() {
           >
             2
           </div>
-        </div>
-        <div className="mx-40 flex flex-grow items-center justify-around gap-10">
-          {otpStatus ? <OtpForm /> : <SignupForm />}
-          <Button href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/google/auth`}>
-          <FontAwesomeIcon icon={faGoogle} className="mx-2" />
-            | Contacts
-          </Button>
-          {/* <Image
-        src="/signup.jpg"
-        alt="Descriptive alt"
-        width={0}
-        height={0}
-        sizes="100vw"
-        className="h-[80dvh] w-auto"
-        priority
-        /> */}
-        </div>
-      </div>
-    </>
-  );
+        </div> */}
+                <div className="mx-40 flex grow items-center justify-around gap-10">
+                    {otpStatus ? <OtpForm /> : <SignupForm />}
+                    <Button
+                        href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/google/auth`}
+                    >
+                        <FontAwesomeIcon icon={faGoogle} className="mx-2" />|
+                        Contacts
+                    </Button>
+                    <Image
+                        src="/signup.jpg"
+                        alt="Descriptive alt"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="h-[80dvh] w-auto"
+                        priority
+                    />
+                </div>
+            </div>
+        </>
+    );
 }
--- fe/app/(app)/dashboard/page.tsx
+++ fe/app/(app)/dashboard/page.tsx
@@ -1,67 +1,109 @@
 "use client";
 
 import { useInitialFetchQuery } from "@features/dashboard/dashboardApi";
-import { setDashboardState } from "@features/dashboard/dashboardSlice";
+import {
+    setContacts,
+    setDashboardState,
+    updateContactIntegration,
+} from "@features/dashboard/dashboardSlice";
 import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import ChatWindow from "@components/ChatWindow";
 import ChatTilesSection from "@components/ChatTilesSection";
+import Button from "@components/Button";
 
 export default function Dashboard() {
-  const { data, isLoading, error } = useInitialFetchQuery();
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (data) {
-      dispatch(setDashboardState({ dashboardState: data.data.messageRecords }));
-      localStorage.setItem("userNumber", data.data.userNumber);
+    const dispatch = useDispatch();
+    const { data, isLoading, error } = useInitialFetchQuery();
+    const dashboardState = useSelector(
+        (state: RootState) => state.dashboard.dashboardState
+    );
+    const contactIntegration = useSelector(
+        (state: RootState) => state.dashboard.contactIntegration
+    );
+    const selectedChat = useSelector(
+        (state: RootState) => state.dashboard.selectedChat
+    );
+    // Update the dashboard state when data is fetched
+    useEffect(() => {
+        if (data) {
+            dispatch(
+                setDashboardState({ dashboardState: data.data.messageRecords })
+            );
+            dispatch(
+                updateContactIntegration({
+                    contactIntegrationStatus: data.data.contactIntergration,
+                })
+            );
+            localStorage.setItem("userNumber", data.data.userNumber);
+            localStorage.setItem(
+                "contacts",
+                JSON.stringify(data.data.contactRecords)
+            );
+            dispatch(
+                setContacts({
+                    contactRecord: data.data.contactRecords.contactMap,
+                })
+            );
+        }
+    }, [data, dispatch]);
+
+    const selectedChatMessage = useMemo(() => {
+        console.log("🔍 Selected Chat ID:", selectedChat);
+        console.log("📦 Dashboard State:", dashboardState);
+
+        if (!selectedChat) {
+            console.log("⚠️ No chat selected");
+            return [];
+        }
+
+        const chat = dashboardState.find((obj) => obj.id === selectedChat);
+        console.log("💬 Found Chat:", chat);
+        console.log("📨 Messages:", chat?.allMessagesOfThisChat);
+
+        return chat?.allMessagesOfThisChat || [];
+    }, [dashboardState, selectedChat]);
+    // Loading While Fetching Data
+    if (isLoading) return <div>Loading...</div>;
+    // Error Handling While Fetching Data
+    if (error) {
+        console.error("Error:", error); // Debug log
+        return <div>Error loading dashboard: {JSON.stringify(error)}</div>;
     }
-  }, [data, dispatch]);
-  const dashboardState = useSelector(
-    (state: RootState) => state.dashboard.dashboardState
-  );
-  const selectedChat = useSelector(
-    (state: RootState) => state.dashboard.selectedChat
-  );
-  // const userId = useSelector((state: RootState) => state.auth.userId);
-  // console.log(`userPage: ${userId}`);
-  // if (userId != "") console.log(`userPage with check: ${userId}`);
-  // Use useMemo to memoize and add debugging
-  const selectedChatMessage = useMemo(() => {
-    console.log("🔍 Selected Chat ID:", selectedChat);
-    console.log("📦 Dashboard State:", dashboardState);
 
-    if (!selectedChat) {
-      console.log("⚠️ No chat selected");
-      return [];
-    }
-
-    const chat = dashboardState.find((obj) => obj.id === selectedChat);
-    console.log("💬 Found Chat:", chat);
-    console.log("📨 Messages:", chat?.allMessagesOfThisChat);
-
-    return chat?.allMessagesOfThisChat || [];
-  }, [dashboardState, selectedChat]);
-  if (isLoading) return <div>Loading...</div>;
-  if (error) {
-    console.error("Error:", error); // Debug log
-    return <div>Error loading dashboard: {JSON.stringify(error)}</div>;
-  }
-
-  return (
-    <div className="flex h-screen w-screen text-black">
-      {/* Left Chat Section */}
-      <ChatTilesSection dashboardState={dashboardState} />
-      {/* Section's Vertical Divider */}
-      {/* <div className="h-full w-0 bg-amber-200"></div> */}
-      {/* Right Side Chat Window Section*/}
-      {selectedChat ? (
-        <ChatWindow messageArr={selectedChatMessage} />
-      ) : (
-        <div className="flex h-full w-full items-center justify-center bg-gray-300 text-gray-400">
-          Select a chat to view messages
+    return (
+        <div className="flex h-screen w-screen text-black">
+            {/* Left Chat Section */}
+            <ChatTilesSection
+                dashboardState={dashboardState}
+                contactIntegration={contactIntegration}
+            />
+            {/* Section's Vertical Divider */}
+            {/* <div className="h-full w-0 bg-amber-200"></div> */}
+            {/* Right Side Chat Window Section*/}
+            {contactIntegration ? (
+                selectedChat ? (
+                    <ChatWindow messageArr={selectedChatMessage} />
+                ) : (
+                    <div className="flex h-full w-full items-center justify-center bg-gray-300 text-gray-400">
+                        Select a chat to view messages
+                    </div>
+                )
+            ) : (
+                <div className="flex h-full w-full flex-col items-center justify-center bg-gray-300 text-gray-400">
+                    You do not have any contacts please import your contact
+                    using a google account
+                    {/* please integrate your contacts using your google account */}
+                    <Button
+                        className={`mt-5 w-fit rounded-lg px-4 py-0 text-base font-normal`}
+                        href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/google/auth`}
+                    >
+                        {" "}
+                        Import Contacts{" "}
+                    </Button>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
