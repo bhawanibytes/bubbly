da6091dcf4702ee992b3ef3e42642f82b3edc14f
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-14 20:21:13 +0530

    connected chat slice

--- fe/src/redux/store.ts
+++ fe/src/redux/store.ts
@@ -3,17 +3,20 @@ import authReducer from "@features/auth/authSlice";
 import { authApi } from "@features/auth/authApi";
 import { dashboardApi } from "@features/dashboard/dashboardApi";
 import dashboardReducer from "@features/dashboard/dashboardSlice";
+import chatReducer from "@features/chats/chatSlice";
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    [authApi.reducerPath]: authApi.reducer,
-    dashboard: dashboardReducer,
-    [dashboardApi.reducerPath]: dashboardApi.reducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(authApi.middleware)
-      .concat(dashboardApi.middleware),
+    reducer: {
+        auth: authReducer,
+        [authApi.reducerPath]: authApi.reducer,
+        dashboard: dashboardReducer,
+        [dashboardApi.reducerPath]: dashboardApi.reducer,
+        chat: chatReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware()
+            .concat(authApi.middleware)
+            .concat(dashboardApi.middleware),
 });
 
 // Infer the type of makeStore
