d20e47b900a5c93867de9907a7e6174377fe4e51
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-16 22:01:05 +0530

    in createDm controller added sending first message functionality, handle finding any avail dm chat or creating if not available and sending first message as well.

--- ws/src/controllers/chats.controllers.ts
+++ ws/src/controllers/chats.controllers.ts
@@ -2,15 +2,12 @@ import { db } from "@db/db"
 import { chats } from "@schema/chats"
 import { users } from "@schema/users"
 import logger from "@configs/logger.config"
-import { and, eq, inArray } from "drizzle-orm"
+import { and, eq, inArray, ne } from "drizzle-orm"
 import { UWSReq, UWSRes } from "@/types/type.uws"
 import { chatMembers } from "@schema/chatMembers"
 import Response from "@shared/types/response.type"
-import {
-  CreateDmChatBody,
-  leaveGroupBody,
-  // SendDmBody,
-} from "@shared/types/chat.type"
+import { CreateDmChatBody, leaveGroupBody } from "@shared/types/body/chat.type"
+import { messages } from "@db/schema/messages"
 // import { messages } from "@schema/messages"
 
 // createChat (1-to-1 or group)
@@ -25,104 +22,212 @@ import {
 
 // create a dm chat
 export async function createDmChat(
-  res: UWSRes,
-  req: UWSReq,
-  body: CreateDmChatBody,
+    res: UWSRes,
+    req: UWSReq,
+    body: CreateDmChatBody,
 ): Promise<Response> {
-  const senderId = res.user.id
-  logger.info(`user from middleware`, res.user)
-  const { receiverNumber } = body
-  //ensure both from and receiverNumber received correctly
-  if (!senderId || !receiverNumber) {
-    return {
-      success: false,
-      status: "400 Bad Request",
-      message: "All arg required",
+    const senderId = res.user.id
+    logger.info(`user from middleware`, res.user)
+    // Todo: message type should be manually checked
+    const { receiverNumber, messageContent, messageType, replyTo } = body
+    //ensure both from and receiverNumber received correctly
+    if (!senderId || !receiverNumber) {
+        return {
+            success: false,
+            status: "400 Bad Request",
+            message: "All arg required",
+        }
     }
-  }
-  try {
-    // finds if receiver has an account, else returns
-    const [receiver] = await db
-      .select()
-      .from(users)
-      .where(
-        and(eq(users.phoneNumber, receiverNumber), eq(users.isVerified, true)),
-      )
-    logger.info(`receiver:`, receiver)
-    if (!receiver) {
-      return {
-        success: false,
-        status: "404 Not Found",
-        message: "User not found",
-      }
-    }
+    try {
+        // finds if receiver has an account, else returns
+        const [receiver] = await db
+            .select()
+            .from(users)
+            .where(
+                and(
+                    eq(users.phoneNumber, receiverNumber),
+                    eq(users.isVerified, true),
+                ),
+            )
+        logger.info(`receiver:`, receiver)
 
-    // return chat.id if chat already there else undefine
-    const checkChatExistance = await db
-      .select({ chatId: chatMembers.chatId })
-      .from(chatMembers)
-      .innerJoin(chats, eq(chatMembers.chatId, chats.id))
-      .where(
-        and(
-          eq(chats.isGroup, false),
-          eq(chatMembers.userId, senderId),
-          inArray(
-            chatMembers.chatId,
-            db
-              .select({ chatId: chatMembers.chatId })
-              .from(chatMembers)
-              .where(eq(chatMembers.userId, receiver.id)),
-          ),
-        ),
-      )
-    logger.info(`Result from checkChatExistance is:`, checkChatExistance)
-    if (1 <= checkChatExistance.length) {
-      return {
-        success: false,
-        status: "409 Conflict",
-        message: "Chat already Existed",
-      }
-    }
+        // return if receiver not available on platform
+        if (!receiver) {
+            return {
+                success: false,
+                status: "404 Not Found",
+                message: "User not found",
+            }
+        }
 
-    // create chat alongwith inserting chat member using sql transaction
-    const chatTransaction = await db.transaction(async (tx) => {
-      const [chatCreated] = await tx
-        .insert(chats)
-        .values({
-          isGroup: false,
-          createdBy: senderId,
+        // check if sender and receiverNumber has some dm chat in common if yes return common chat also
+        const commonChatExits = await db.transaction(async (tx) => {
+            // all chats non-group of sender
+            const senderAllMembership = await tx
+                .select({ chatId: chatMembers.chatId })
+                .from(chatMembers)
+                .innerJoin(chats, eq(chatMembers.chatId, chats.id))
+                .where(
+                    and(
+                        eq(chatMembers.userId, senderId),
+                        eq(chats.isGroup, false),
+                    ),
+                )
+            // return status false if no chat available for sender
+            if (!senderAllMembership.length)
+                return { status: false, commonId: "" }
+
+            const membershipList = senderAllMembership.map((c) => c.chatId)
+
+            // check if receiver has membership entry for the non-group chat of sender
+            const commanChat = await tx
+                .select({ chatId: chatMembers.chatId })
+                .from(chatMembers)
+                .where(
+                    and(
+                        inArray(chatMembers.chatId, membershipList),
+                        eq(chatMembers.userId, receiver.id),
+                    ),
+                )
+            // return status false if there is no dm chat b/w sender and receiver
+            if (commanChat.length === 0) return { status: false, commonId: "" }
+            // return status true and chat id if dm available already
+            return { status: true, commonId: commanChat[0].chatId }
         })
-        .returning()
-      await tx.insert(chatMembers).values([
-        {
-          chatId: chatCreated.id,
-          userId: senderId,
-        },
-        {
-          chatId: chatCreated.id,
-          userId: receiver.id,
-        },
-      ])
-      return chatCreated
-    })
-    logger.info(`chatTransaction:`, chatTransaction)
-    if (chatTransaction) {
-      return {
-        success: true,
-        status: "201 Created",
-        message: "Chat created.",
-        data: { chatId: chatTransaction.id },
-      }
+
+        logger.info(`Result from commonChatExits is:`, commonChatExits)
+        if (commonChatExits.status) {
+            // send messge on that chat id
+            const sendMessageTx = await db.transaction(async (tx) => {
+                await tx.insert(messages).values({
+                    chatId: commonChatExits.commonId,
+                    senderId: senderId,
+                    content: messageContent,
+                    messageType,
+                    replyTo,
+                })
+
+                // fetch all details of the chat
+                const [chatRecords] = await tx.query.chats.findMany({
+                    where: eq(chats.id, commonChatExits.commonId),
+                    with: {
+                        allMessagesOfThisChat: {
+                            with: {
+                                senderOfThisMessage: {
+                                    columns: {
+                                        phoneNumber: true,
+                                    },
+                                },
+                            },
+                            orderBy: (messages, { asc }) => [
+                                asc(messages.createdAt),
+                            ],
+                        },
+                        membersOfThisChat: {
+                            where: ne(chatMembers.userId, senderId),
+                            with: {
+                                userToWhichThisMembershipBelongTo: {
+                                    columns: {
+                                        phoneNumber: true,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    orderBy: (chats, { desc }) => [
+                        desc(chats.lastMessageTimestamp),
+                    ],
+                })
+                return chatRecords
+            })
+            return {
+                success: false,
+                status: "202 Accepted",
+                message: "Message sent on your already created DM",
+                data: sendMessageTx,
+            }
+        }
+
+        // create chat alongwith inserting chat member and message in that chat using sql transaction and retrive all details of chat
+        const chatTransaction = await db.transaction(async (tx) => {
+            // create chat
+            const [chatCreated] = await tx
+                .insert(chats)
+                .values({
+                    isGroup: false,
+                    createdBy: senderId,
+                })
+                .returning()
+            // insert membership of both sender and receiver
+            await tx.insert(chatMembers).values([
+                {
+                    chatId: chatCreated.id,
+                    userId: senderId,
+                },
+                {
+                    chatId: chatCreated.id,
+                    userId: receiver.id,
+                },
+            ])
+            // insert message in chat
+            await tx.insert(messages).values({
+                chatId: chatCreated.id,
+                senderId: senderId,
+                content: messageContent,
+                messageType,
+                replyTo,
+            })
+            // fetch all details of the chat
+            const [chatRecords] = await tx.query.chats.findMany({
+                where: eq(chats.id, chatCreated.id),
+                with: {
+                    allMessagesOfThisChat: {
+                        with: {
+                            senderOfThisMessage: {
+                                columns: {
+                                    phoneNumber: true,
+                                },
+                            },
+                        },
+                        orderBy: (messages, { asc }) => [
+                            asc(messages.createdAt),
+                        ],
+                    },
+                    membersOfThisChat: {
+                        where: ne(chatMembers.userId, senderId),
+                        with: {
+                            userToWhichThisMembershipBelongTo: {
+                                columns: {
+                                    phoneNumber: true,
+                                },
+                            },
+                        },
+                    },
+                },
+                orderBy: (chats, { desc }) => [
+                    desc(chats.lastMessageTimestamp),
+                ],
+            })
+            return chatRecords
+        })
+        logger.info(`chatTransaction:`, chatTransaction)
+        if (chatTransaction) {
+            return {
+                success: true,
+                status: "201 Created",
+                message: "Chat created.",
+                data: chatTransaction,
+            }
+        }
+        throw new Error("Unexpected code path")
+    } catch (error) {
+        logger.error(`handler crashed with the error:`, error)
+        return {
+            success: false,
+            status: "500 Internal Server Error",
+            message: "Something Went Wrong",
+        }
     }
-    throw new Error("Unexpected code path")
-  } catch (error) {
-    logger.error(`handler crashed with the error:`, error)
-    return {
-      success: false,
-      status: "500 Internal Server Error",
-      message: "Something Went Wrong",
-    }
-  }
 }
 
 // // send a dm message
@@ -209,113 +314,115 @@ export async function createDmChat(
 
 // fetch all/few chats of an user
 export async function fetchChats(res: UWSRes, req: UWSReq): Promise<Response> {
-  const userId = res.user.id
-  const rawChatList = await db.query.users.findMany({
-    where: eq(users.id, userId),
-    with: {
-      allChatMembershipOfUser: {
-        columns: {
-          chatId: true,
+    const userId = res.user.id
+    const rawChatList = await db.query.users.findMany({
+        where: eq(users.id, userId),
+        with: {
+            allChatMembershipOfUser: {
+                columns: {
+                    chatId: true,
+                },
+                orderBy: (chatMembers, { desc }) => [
+                    desc(chatMembers.createdAt),
+                ],
+            },
+            // chatMembers: {      orderBy: (chatMembers, { desc }) => [desc(chatMembers.joinedAt)],    },    messages:       orderBy: (messages, { desc }) => [desc(messages.createdAt)],    },
         },
-        orderBy: (chatMembers, { desc }) => [desc(chatMembers.createdAt)],
-      },
-      // chatMembers: {      orderBy: (chatMembers, { desc }) => [desc(chatMembers.joinedAt)],    },    messages:       orderBy: (messages, { desc }) => [desc(messages.createdAt)],    },
-    },
-  })
-  // .select({
-  //   chatId: chats.id,
-  //   isGroup: chats.isGroup,
-  //   groupName: chats.groupName,
-  // })
-  // .from(chats)
-  // .innerJoin(chatMembers, eq(chats.id, chatMembers.chatId))
-  // .where(eq(chatMembers.userId, userId))
+    })
+    // .select({
+    //   chatId: chats.id,
+    //   isGroup: chats.isGroup,
+    //   groupName: chats.groupName,
+    // })
+    // .from(chats)
+    // .innerJoin(chatMembers, eq(chats.id, chatMembers.chatId))
+    // .where(eq(chatMembers.userId, userId))
 
-  // const dmChatId = rawChatList
-  //   .filter((chat) => chat.isGroup === false)
-  //   .map((chat) => chat.chatId)
+    // const dmChatId = rawChatList
+    //   .filter((chat) => chat.isGroup === false)
+    //   .map((chat) => chat.chatId)
 
-  // const listWithNumbers = await db
-  //   .select({ chatId: chatMembers.chatId, userNumber: users.phoneNumber })
-  //   .from(chatMembers)
-  //   .innerJoin(users, eq(users.id, chatMembers.userId))
-  //   .where(
-  //     and(
-  //       inArray(chatMembers.chatId, dmChatId),
-  //       ne(chatMembers.userId, userId),
-  //     ),
-  //   )
+    // const listWithNumbers = await db
+    //   .select({ chatId: chatMembers.chatId, userNumber: users.phoneNumber })
+    //   .from(chatMembers)
+    //   .innerJoin(users, eq(users.id, chatMembers.userId))
+    //   .where(
+    //     and(
+    //       inArray(chatMembers.chatId, dmChatId),
+    //       ne(chatMembers.userId, userId),
+    //     ),
+    //   )
 
-  return {
-    success: true,
-    status: "200 OK",
-    message: "Here is your all chats",
-    data: rawChatList,
-  }
+    return {
+        success: true,
+        status: "200 OK",
+        message: "Here is your all chats",
+        data: rawChatList,
+    }
 }
 
 // leave a group
 export async function leaveGroup(
-  res: UWSRes,
-  req: UWSReq,
-  body: leaveGroupBody,
+    res: UWSRes,
+    req: UWSReq,
+    body: leaveGroupBody,
 ): Promise<Response> {
-  const userId = res.user.id
-  const { chatId } = body
-  // return if any of chatId , userId not provided
-  if (!userId || !chatId) {
-    return {
-      success: false,
-      status: "400 Bad Request",
-      message: "All arg required",
+    const userId = res.user.id
+    const { chatId } = body
+    // return if any of chatId , userId not provided
+    if (!userId || !chatId) {
+        return {
+            success: false,
+            status: "400 Bad Request",
+            message: "All arg required",
+        }
     }
-  }
-  try {
-    // check if user is member of that chat, if yes retrieved the Id from chatMember, if not
-    const [userGroupMembership] = await db
-      .select({ membershipId: chatMembers.chatId })
-      .from(chatMembers)
-      .innerJoin(chats, eq(chatMembers.chatId, chats.id))
-      .where(
-        and(
-          eq(chats.isGroup, true),
-          eq(chatMembers.chatId, chatId),
-          eq(chatMembers.userId, userId),
-        ),
-      )
-      .limit(1)
-    if (!userGroupMembership) {
-      return {
-        success: false,
-        status: "400 Bad Request",
-        message: "Don't this you will be caught",
-      }
-    }
-    const [userLeftGroup] = await db
-      .delete(chatMembers)
-      .where(
-        and(
-          eq(chatMembers.chatId, userGroupMembership.membershipId),
-          eq(chatMembers.userId, userId),
-        ),
-      )
-      .returning({
-        deletedId: chatMembers.chatId,
-        deletedUser: chatMembers.userId,
-      })
+    try {
+        // check if user is member of that chat, if yes retrieved the Id from chatMember, if not
+        const [userGroupMembership] = await db
+            .select({ membershipId: chatMembers.chatId })
+            .from(chatMembers)
+            .innerJoin(chats, eq(chatMembers.chatId, chats.id))
+            .where(
+                and(
+                    eq(chats.isGroup, true),
+                    eq(chatMembers.chatId, chatId),
+                    eq(chatMembers.userId, userId),
+                ),
+            )
+            .limit(1)
+        if (!userGroupMembership) {
+            return {
+                success: false,
+                status: "400 Bad Request",
+                message: "Don't this you will be caught",
+            }
+        }
+        const [userLeftGroup] = await db
+            .delete(chatMembers)
+            .where(
+                and(
+                    eq(chatMembers.chatId, userGroupMembership.membershipId),
+                    eq(chatMembers.userId, userId),
+                ),
+            )
+            .returning({
+                deletedId: chatMembers.chatId,
+                deletedUser: chatMembers.userId,
+            })
 
-    return {
-      success: true,
-      status: "202 Accepted",
-      message: "You left the group",
-      data: userLeftGroup,
+        return {
+            success: true,
+            status: "202 Accepted",
+            message: "You left the group",
+            data: userLeftGroup,
+        }
+    } catch (error) {
+        logger.error(`handler crashed with the error:`, error)
+        return {
+            success: false,
+            status: "500 Internal Server Error",
+            message: "Something Went Wrong",
+        }
     }
-  } catch (error) {
-    logger.error(`handler crashed with the error:`, error)
-    return {
-      success: false,
-      status: "500 Internal Server Error",
-      message: "Something Went Wrong",
-    }
-  }
 }
