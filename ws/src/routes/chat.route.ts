84400b779d37bd96a40c6f9b1c9b524154c2e312
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-16 21:46:19 +0530

    added create dm and send message controller to route

--- ws/src/routes/chat.route.ts
+++ ws/src/routes/chat.route.ts
@@ -1,17 +1,10 @@
 import { AppType } from "../types/type.uws"
-import {
-  createDmChat,
-  fetchChats,
-  // sendDm,
-} from "@controllers/chats.controllers"
+import { createDmChat, fetchChats } from "@controllers/chats.controllers"
 import authMiddleware from "@middlewares/auth.middleware"
 import wrapper from "@wrappers/asyncJsonCorsMiddleware.wrapper"
 import asyncCorsMiddlewareWrapper from "@/wrappers/asyncCorsMiddleware.wrapper"
 
 export default function registerChatRoutes(app: AppType) {
-  app.post("/newchat", authMiddleware(wrapper(createDmChat)))
-  app.get("/mychats", authMiddleware(asyncCorsMiddlewareWrapper(fetchChats)))
-  // app.post("/send-dm", authMiddleware(wrapper(sendDm)))
-  //pending
-  // app.post("/delete-dm", authMiddleware(wrapper()))
+    app.post("/create-dm-and-message", authMiddleware(wrapper(createDmChat)))
+    app.get("/my-chats", authMiddleware(asyncCorsMiddlewareWrapper(fetchChats)))
 }
