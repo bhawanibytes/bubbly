56e9d30632a6c08ddb47e318bae5786728612f68
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-16 21:53:10 +0530

    just formatting changed

--- ws/src/middlewares/auth.middleware.ts
+++ ws/src/middlewares/auth.middleware.ts
@@ -1,32 +1,34 @@
-import { env } from "../configs/env.config"
+import { env } from "@configs/env.config"
 import { UWSReq, UWSRes } from "../types/type.uws"
-import logger from "../configs/logger.config"
-import cookieParser from "../utils/cookieParser"
+import logger from "@configs/logger.config"
+import cookieParser from "@utils/cookieParser"
 import jwt from "jsonwebtoken"
 
 export default function authMiddleware<T = any>(
-  next: (res: UWSRes, req: UWSReq) => void,
+    next: (res: UWSRes, req: UWSReq) => void,
 ) {
-  return (res: UWSRes, req: UWSReq) => {
-    try {
-      const cookies = cookieParser(req)
-      logger.info("cookies from auth.middleware.ts:", { cookies })
-      if (!cookies.accessToken) throw new Error("Access token missing")
+    return (res: UWSRes, req: UWSReq) => {
+        try {
+            const cookies = cookieParser(req)
+            logger.info("cookies from auth.middleware.ts:", { cookies })
+            if (!cookies.accessToken) throw new Error("Access token missing")
 
-      const verifiedData = jwt.verify(cookies.accessToken, env.JWT_SECRET)
-      logger.info(`Verified Data from auth.middleware.ts:`, { verifiedData })
+            const verifiedData = jwt.verify(cookies.accessToken, env.JWT_SECRET)
+            logger.info(`Verified Data from auth.middleware.ts:`, {
+                verifiedData,
+            })
 
-      res.user = verifiedData
-      // calling the next handler that is passed
-      next(res, req)
-    } catch (error: any) {
-      logger.error(`JWT Error: ${error.message}`, error)
+            res.user = verifiedData
+            // calling the next handler that is passed
+            next(res, req)
+        } catch (error: any) {
+            logger.error(`JWT Error: ${error.message}`, error)
 
-      if (error.name === "TokenExpiredError") {
-        res.writeStatus("401 Unauthorized").end("Token expired")
-      } else {
-        res.writeStatus("401 Unauthorized").end("Invalid token")
-      }
+            if (error.name === "TokenExpiredError") {
+                res.writeStatus("401 Unauthorized").end("Token expired")
+            } else {
+                res.writeStatus("401 Unauthorized").end("Invalid token")
+            }
+        }
     }
-  }
 }
