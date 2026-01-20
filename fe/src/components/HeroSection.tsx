6f6a5630983c5469fc725f00cc916abddae8339a
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-20 09:18:21 +0530

    feat: update Footer and Navbar components to use environment variables for links

--- fe/src/components/HeroSection.tsx
+++ fe/src/components/HeroSection.tsx
@@ -1,34 +1,35 @@
 import Link from "next/link";
 
 export default function HeroSection() {
-  return (
-    <>
-      <div className="relative z-0 m-4 h-[30rem] w-[58rem]">
-        <div
-          style={{
-            backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.4)), url('/heroImage.png')`,
-          }}
-          className="hero-image flex min-h-[30rem] min-w-[58rem] flex-col justify-end gap-8 rounded-2xl bg-[url('/heroImage.png')] bg-cover bg-center bg-no-repeat px-10 pb-10 text-white"
-        >
-          <div className="headings flex flex-col gap-2">
-            <h1 className="text-5xl leading-tight font-black tracking-[-0.033em]">
-              Connect Instantly, Communicate Seamlessly
-            </h1>
-            <h2 className="text-base font-normal text-[#F2F2F2]">
-              Experience real-time messaging with ChatApp. Our platform offers
-              secure, reliable, and feature-rich communication tools for
-              individuals and teams. Stay connected with friends, family, and
-              colleagues effortlessly.
-            </h2>
-          </div>
-          <Link
-            href="/signup"
-            className="flex h-12 w-[8.438rem] items-center justify-center rounded-4xl bg-black text-base font-bold"
-          >
-            Get Started
-          </Link>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="relative z-0 m-4 h-120 w-232">
+                <div
+                    style={{
+                        backgroundImage: `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.4)), url('/heroImage.png')`,
+                    }}
+                    className="hero-image flex min-h-120 min-w-232 flex-col justify-end gap-8 rounded-2xl bg-[url('/heroImage.png')] bg-cover bg-center bg-no-repeat px-10 pb-10 text-white"
+                >
+                    <div className="headings flex flex-col gap-2">
+                        <h1 className="text-5xl leading-tight font-black tracking-[-0.033em]">
+                            Connect Instantly, Communicate Seamlessly
+                        </h1>
+                        <h2 className="text-base font-normal text-[#F2F2F2]">
+                            Experience real-time messaging with ChatApp. Our
+                            platform offers secure, reliable, and feature-rich
+                            communication tools for individuals and teams. Stay
+                            connected with friends, family, and colleagues
+                            effortlessly.
+                        </h2>
+                    </div>
+                    <Link
+                        href="/signup"
+                        className="flex h-12 w-[8.438rem] items-center justify-center rounded-4xl bg-black text-base font-bold"
+                    >
+                        Get Started
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
 }
--- fe/src/components/Navbar.tsx
+++ fe/src/components/Navbar.tsx
@@ -2,51 +2,51 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
-  return (
-    <div className="navContainer flex min-h-16 items-center justify-between text-black">
-      <div className="logo">
-        <Link href="/" className="flex gap-4">
-          <Image
-            className=""
-            src="/logo.svg"
-            alt="Bubbly logo"
-            width={16}
-            height={16}
-            priority
-          />
-          <h1 className="text-lg font-bold">Dap Connect</h1>
-        </Link>
-      </div>
-      <nav className="navlinks flex items-center gap-8">
-        <ul className="flex gap-9">
-          <Link href="/product" className="text-sm font-medium">
-            {" "}
-            Product{" "}
-          </Link>
-          <Link href="/features" className="text-sm font-medium">
-            {" "}
-            Features{" "}
-          </Link>
-          <Link href="/pricing" className="text-sm font-medium">
-            {" "}
-            Pricing{" "}
-          </Link>
-        </ul>
-        <div className="flex gap-2 font-bold">
-          <Link
-            href="/signup"
-            className="flex h-10 w-[5.375rem] items-center justify-center rounded-3xl bg-black text-sm text-white"
-          >
-            Sign Up
-          </Link>
-          <Link
-            href="/login"
-            className="flex h-10 w-[5.375rem] items-center justify-center rounded-3xl bg-[#F2F2F2] text-sm"
-          >
-            Log in
-          </Link>
+    return (
+        <div className="navContainer flex min-h-16 items-center justify-between text-black">
+            <div className="logo">
+                <Link href="/" className="flex gap-4">
+                    <Image
+                        className=""
+                        src="/logo.svg"
+                        alt="Bubbly logo"
+                        width={16}
+                        height={16}
+                        priority
+                    />
+                    <h1 className="text-lg font-bold">Bubbly</h1>
+                </Link>
+            </div>
+            <nav className="navlinks flex items-center gap-8">
+                {/* <ul className="flex gap-9">
+                    <Link href="/product" className="text-sm font-medium">
+                        {" "}
+                        Product{" "}
+                    </Link>
+                    <Link href="/features" className="text-sm font-medium">
+                        {" "}
+                        Features{" "}
+                    </Link>
+                    <Link href="/pricing" className="text-sm font-medium">
+                        {" "}
+                        Pricing{" "}
+                    </Link>
+                </ul> */}
+                <div className="flex gap-2 font-bold">
+                    <Link
+                        href="/signup"
+                        className="flex h-10 w-21.5 items-center justify-center rounded-3xl bg-black text-sm text-white"
+                    >
+                        Sign Up
+                    </Link>
+                    <Link
+                        href="/login"
+                        className="flex h-10 w-21.5 items-center justify-center rounded-3xl bg-[#F2F2F2] text-sm"
+                    >
+                        Log in
+                    </Link>
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+    );
 }
