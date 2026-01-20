8b6151f70bae4d4628c280cd28fa35faa60fefce
Bhawani Singh <bhawanibytes@gmail.com>
2026-01-20 09:12:10 +0530

    fix: correct import path for auth types in authApi

--- fe/src/features/auth/authApi.ts
+++ fe/src/features/auth/authApi.ts
@@ -2,41 +2,41 @@ import { baseQuery } from "@baseApi";
 import Response from "@shared/types/response.type";
 import { createApi } from "@reduxjs/toolkit/query/react";
 import {
-  LoginBody,
-  SignupBody,
-  VerifySignupBody,
-} from "@shared/types/auth.type";
+    LoginBody,
+    SignupBody,
+    VerifySignupBody,
+} from "@shared/types/body/auth.type";
 
 export const authApi = createApi({
-  reducerPath: "authApi", // optional: name in store
-  baseQuery: baseQuery,
-  endpoints: (builder) => ({
-    register: builder.mutation<Response, SignupBody>({
-      query: (data) => ({
-        url: "/signup",
-        method: "POST",
-        body: data,
-      }),
+    reducerPath: "authApi", // optional: name in store
+    baseQuery: baseQuery,
+    endpoints: (builder) => ({
+        register: builder.mutation<Response, SignupBody>({
+            query: (data) => ({
+                url: "/signup",
+                method: "POST",
+                body: data,
+            }),
+        }),
+        verifyUser: builder.mutation<Response, VerifySignupBody>({
+            query: (data) => ({
+                url: "/verify-signup",
+                method: "POST",
+                body: data,
+            }),
+        }),
+        loginUser: builder.mutation<Response, LoginBody>({
+            query: (data) => ({
+                url: "/login",
+                method: "POST",
+                body: data,
+            }),
+        }),
     }),
-    verifyUser: builder.mutation<Response, VerifySignupBody>({
-      query: (data) => ({
-        url: "/verify-signup",
-        method: "POST",
-        body: data,
-      }),
-    }),
-    loginUser: builder.mutation<Response, LoginBody>({
-      query: (data) => ({
-        url: "/login",
-        method: "POST",
-        body: data,
-      }),
-    }),
-  }),
 });
 
 export const {
-  useRegisterMutation,
-  useVerifyUserMutation,
-  useLoginUserMutation,
+    useRegisterMutation,
+    useVerifyUserMutation,
+    useLoginUserMutation,
 } = authApi;
